8e597e509d03ca3d720d71a3077039db6abff744
Trung Nguyen <nguyent@users.noreply.github.com>
2017-02-12 10:48:44 +0000

    improve ECS task definition registration

--- aws_ecs/register-task-definition.sh
+++ aws_ecs/register-task-definition.sh
@@ -3,6 +3,9 @@
 IMAGE=$1
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TMPFILE=$(mktemp)
-cat $DIR/design-prototypes.json | sed -e s/\{\{IMAGE\}\}/$IMAGE/ > $TMPFILE
+cat $DIR/task-definition.json   | sed -e s/\{\{IMAGE\}\}/$IMAGE/ \
+                                | sed -e s/\{\{APP_NAME\}\}/$APP_NAME/ \
+                                | sed -e s/\{\{AWS_REGION\}\}/$AWS_REGION/ \
+                                > $TMPFILE
 aws ecs register-task-definition --cli-input-json file://$TMPFILE
 rm $TMPFILE
